1388805d60c930de7f5958dd56d5ba33a91b2f90
Nazg0r <gorchinshiy83@gmail.com>
2024-02-28 20:12:34 +0200

    Add CL arguments to program

--- Markdown display/Program.cs
+++ Markdown display/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace ConsoleParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Missing argiments");
+                return;
+            }
+
+            string path = args[0];
+            string? destinationPath;
+
+            if (!File.Exists(path))
+            {
+                Console.Error.WriteLine("Invalid file path");
+                return;
+            }
+
+            if (args.Length == 1)
+            {
+                Console.WriteLine(path);
+                return;
+            }
+
+            if (args.Length == 3 && args[1] == "--out")
+            {
+                destinationPath = args[2];
+
+                Console.WriteLine(destinationPath);
+                return;
+            }
+
+            Console.Error.WriteLine("Wrong number of parameters");
+            return;
+
+        }
+    }
+}
